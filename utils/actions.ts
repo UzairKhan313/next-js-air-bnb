ea06f7249ee3bdb5963e36c9e3646aceec932d6d
UzairKhan313 <uzairkhaan2003@gmail.com>
2024-12-30 18:45:08 +0500

    Done with updating profile and modifying zod schema

--- utils/actions.ts
+++ utils/actions.ts
@@ -3,7 +3,21 @@ import db from "@/utils/db";
 import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
-import { profileSchema } from "./schemas";
+import { profileSchema, validateWithZodSchema } from "./schemas";
+
+const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) throw new Error("You must be logged in to access this route.");
+  if (!user.privateMetadata.hasProfile) redirect("/profile/create");
+  return user;
+};
+
+const renderError = (error: unknown): { message: string } => {
+  console.log(error);
+  return {
+    message: error instanceof Error ? error.message : "An error occurred",
+  };
+};
 
 export const createProfileAction = async (
   _prevState: any,
@@ -13,7 +27,7 @@ export const createProfileAction = async (
     const user = await currentUser();
     if (!user) throw new Error("Please login to create a profile");
     const rowData = Object.fromEntries(formData);
-    const validateFields = profileSchema.parse(rowData);
+    const validateFields = validateWithZodSchema(profileSchema, rowData);
 
     await db.profile.create({
       data: {
@@ -36,14 +50,12 @@ export const createProfileAction = async (
     });
     // return { message: "Profile created" };
   } catch (error: any) {
-    return {
-      message: error instanceof Error ? error.message : "An error occurred",
-    };
+    renderError(error);
   }
   redirect("/");
 };
 
-// GET method to get user image
+// GET Request to get user image
 export const fetchProfileImage = async () => {
   const user = await currentUser();
   if (!user) return null;
@@ -57,4 +69,37 @@ export const fetchProfileImage = async () => {
     },
   });
   return profile?.profileImage;
+};
+// GET Request to get user profile
+export const fetchProfile = async () => {
+  const user = await getAuthUser();
+  const profile = db.profile.findUnique({
+    where: {
+      clerkId: user.id,
+    },
+  });
+  if (!profile) redirect("/profile/create");
+  return profile;
+};
+
+export const updateProfileAction = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  const user = await getAuthUser();
+  try {
+    const rowData = Object.fromEntries(formData);
+    const validatedFields = validateWithZodSchema(profileSchema, rowData);
+    await db.profile.update({
+      where: {
+        clerkId: user.id,
+      },
+      data: validatedFields,
+    });
+
+    revalidatePath("/profile");
+    return { message: "Profile updated successfully." };
+  } catch (error: any) {
+    return renderError(error);
+  }
 };
--- utils/schemas.ts
+++ utils/schemas.ts
@@ -3,7 +3,25 @@ import { ZodSchema } from "zod";
 
 export const profileSchema = z.object({
   // firstName:z.string().max(5,{message:"max length is 5."})
-  firstName: z.string().min(3),
-  username: z.string().min(3),
-  lastName: z.string().min(3),
+  firstName: z
+    .string()
+    .min(3, { message: "First name must be at least 3 characters long" }),
+  username: z
+    .string()
+    .min(3, { message: "username must be at least 3 characters long" }),
+  lastName: z
+    .string()
+    .min(3, { message: "last name must be at least 3 characters long" }),
 });
+
+export function validateWithZodSchema<T>(
+  schema: ZodSchema<T>,
+  data: unknown
+): T {
+  const result = schema.safeParse(data);
+  if (!result.success) {
+    const errors = result.error.errors.map((err) => err.message);
+    throw new Error(errors.join(", "));
+  }
+  return result.data;
+}
