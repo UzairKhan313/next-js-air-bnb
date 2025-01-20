cebdd8a2e2458a5522ad62d41f9b5452d0af8116
UzairKhan313 <uzairkhaan2003@gmail.com>
2025-01-20 10:37:02 +0500

    fixing un-used varaible error

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,9 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "ogiqwcfmvrcndifrcgkl.supabase.co" },
     ],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
