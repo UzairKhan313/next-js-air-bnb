767f7705061e529b2f565262464e4ef3848f8825
UzairKhan313 <uzairkhaan2003@gmail.com>
2025-01-20 10:45:23 +0500

    still working on search params bug in app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,12 @@ import CategoriesList from "@/components/home/CategoriesList";
 import PropertiesContainer from "@/components/home/PropertiesContainer";
 import { Suspense } from "react";
 
-const HomePage = async ({
+const HomePage = ({
   searchParams,
 }: {
   searchParams: { category?: string; search: string };
 }) => {
-  const searchParameters = await searchParams;
-  const { category, search } = searchParameters;
+  const { category, search } = searchParams;
   return (
     <section>
       <CategoriesList category={category} search={search} />
